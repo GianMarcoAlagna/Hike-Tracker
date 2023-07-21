f7e78ceeae5258e755ff9d0ab1d1ec71759fd5cc
GianMarcoAlagna <marcoalagna1@gmail.com>
2023-07-21 14:46:28 -0500

    before-pull

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.text-form').addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const long = document.querySelector('#long').value;
-        const lat = document.querySelector('#lat').value;
-        const url = `https://api.sunrisesunset.io/json?lat=${lat}&lng=${long}`;
+        const long = document.querySelector('#long');
+        const lat = document.querySelector('#lat');
+        const url = `https://api.sunrisesunset.io/json?lat=${lat.value}&lng=${long.value}`;
         fetch(url)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            const info = document.createElement('div');
-            // info.className = 'info-box'; this works too, just less fancy
-            info.classList.add('info-box');
-            const goldenHour = document.createElement('h3');
-            goldenHour.innerText = data.results.golden_hour;
-            info.appendChild(goldenHour);
-            document.body.appendChild(info);
+            const form = document.querySelector('.text-form');
+            form.style.animation = 'close-form 1s';
+            
+            setTimeout(() => {
+                form.style.display = 'none';
+                const info = document.createElement('div');
+                info.classList.add('info-box');
+
+                const goldenHour = document.createElement('h3');
+                goldenHour.innerText = data.results.golden_hour;
+                info.appendChild(goldenHour);
+                document.body.appendChild(info);
+            }, 1001);
         })
         .catch(error => console.error('Error:', error))
         // display sunrise, golden hour, and sunset
