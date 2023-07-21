8e565898171513b8c2e8dd37ddf99e6e337bb2e6
GianMarcoAlagna <marcoalagna1@gmail.com>
2023-07-21 02:46:31 -0500

    added some CSS and basic functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.text-form').addEventListener('submit', (e) => {
         e.preventDefault();
+
         const long = document.querySelector('#long').value;
         const lat = document.querySelector('#lat').value;
         const url = `https://api.sunrisesunset.io/json?lat=${lat}&lng=${long}`;
@@ -8,6 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            const info = document.createElement('div');
+            // info.className = 'info-box'; this works too, just less fancy
+            info.classList.add('info-box');
+            const goldenHour = document.createElement('h3');
+            goldenHour.innerText = data.results.golden_hour;
+            info.appendChild(goldenHour);
+            document.body.appendChild(info);
         })
         .catch(error => console.error('Error:', error))
         // display sunrise, golden hour, and sunset
